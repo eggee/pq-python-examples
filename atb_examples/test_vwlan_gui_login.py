5537098ef88ab8a6c615eb57f9ef3bde53b4c65f
eggee <timothy.eggenberger@adtran.com>
2017-04-13 13:48:20 -0500

    first commit

---
+++ atb_examples/test_vwlan_gui_login.py
@@ -0,0 +1,11 @@
+import time
+
+
+def test_a(record_xml_property):
+    record_xml_property('qc-step', 'test a')
+
+def test_b(record_xml_property):
+    record_xml_property('qc-step', 'test b')
+
+def test_c(record_xml_property):
+    record_xml_property('qc-step', 'test c')
